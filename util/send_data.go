27e7c0af4dad55d87fd0d02fc1225661f23b492a
“farabi1233” <hosnain.ahammad@gmail.com>
2025-08-14 15:53:07 +0600

    Add DD debug function to send_data.go
    Introduces a DD function for debugging that prints a value and exits the program, similar to the dd() pattern. Useful for quick inspection during development.

--- util/send_data.go
+++ util/send_data.go
@@ -2,11 +2,18 @@ package util
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 )
 
 func SendData(w http.ResponseWriter, data interface{}, statusCode int) {
 	w.WriteHeader(statusCode) // Set response status to Created
 	encoder := json.NewEncoder(w)
 	encoder.Encode(data) // Encode the new product as JSON and write to response
+}
+
+func DD(v interface{}) {
+	fmt.Printf("%+v\n", v)
+	os.Exit(1) // stops program like dd()
 }
